44c1bbef3742564878d8257ede08aaaaf92e4b26
Allain Lalonde <allain@machete.ca>
2010-02-10 15:55:42 -0500

    Initial implementation of firefox animation. Boo firefox's Missing animation properties

--- zoomweb.js
+++ zoomweb.js
@@ -30,10 +30,14 @@ $(function() {
     var oldCSS = $.fn.css;
     $.fn.css = function(arg1, arg2) {
       if (typeof(arg1) == "string") {
-        var newArg1 = {}
-        newArg1[arg1] = arg2;
-        arg1 = newArg1;
-        arg2 = null;
+        if (typeof(arg2) == 'undefined') {
+          return oldCSS.apply(this, [arg1, arg2]);
+        } else {
+          var newArg1 = {}
+          newArg1[arg1] = arg2;
+          arg1 = newArg1;
+          arg2 = null;
+        }
       }
 
       arg1 = css3(arg1);
@@ -60,10 +64,140 @@ $(function() {
         height:(size.height*(1/s)),
         overflow: "hidden"
       });
+
+      $e.data("scale", s);
+      $e.data("offset", $e.offset());
     }
 
     return this;
   };
+
+  var $currentTarget = null;
+  $.zoomTo = function (target) {
+    var $target = $(target);    
+
+    var offset = $target.data("offset");   
+
+    var scale = $target.data("scale");
+  
+    var $body = $("body");
+
+    $(".dynamicCSS").remove();
+
+    if ($.browser.webkit) {
+      $body.append("<style type='text/css' class='dynamicCSS'>"+
+        ".grow {"+
+        emitProperties(css3({
+          "transform-origin": offset.left+"px "+offset.top+"px",
+          "animation-name": "grow-anim"
+        }))+
+        "}\n"+
+        "@-webkit-keyframes grow-anim {\n"+
+        "0% { -webkit-transform: translate(0, 0); scale(1)}\n "+
+        "100% { -webkit-transform: translate("+(-offset.left)+"px, "+(-offset.top)+"px) scale("+scale+");  }\n"+
+        "}\n "+
+        ".zoomed {\n" +
+        emitProperties(css3({
+          "transform-origin": offset.left + "px "+offset.top + "px",
+          "transform": "translate("+(-offset.left)+"px,"+(-offset.top)+"px) scale("+scale+")"
+        }))+ "}\n</style>");
+
+      $body.addClass("grow").removeClass("grown").removeClass("shrink");
+      setTimeout(function() {
+        $body.removeClass("grow").addClass("zoomed");
+        $target.addClass("grown");
+      }, 1000);
+    } else if ($.browser.mozilla) {
+      $body.append("<style type='text/css' class='dynamicCSS'>"+
+        ".grow {"+
+        emitProperties(css3({
+          "transform-origin": offset.left+"px "+offset.top+"px"
+        }))+
+        "}\n"+
+        ".zoomed {\n" +
+        emitProperties(css3({
+          "transform-origin": offset.left + "px "+offset.top + "px",
+          "transform": "translate("+(-offset.left)+"px,"+(-offset.top)+"px) scale("+scale+")"
+        }))+ 
+        "}\n</style>");
+
+        var startTime = new Date().getTime();
+        var zoomIntervalId = setInterval(function() {        
+          var ratio = (new Date().getTime()-startTime)/1000;
+          $body.append("<style type='text/css' class='dynamicCSS'>"+
+          ".grow {"+
+            emitProperties(css3({
+              "transform-origin": offset.left+"px "+offset.top+"px",
+              "transform": "translate("+(-offset.left*ratio)+"px,"+(-offset.top*ratio)+"px) scale("+(scale * ratio)+")"
+            }))+ "}");
+        }, 25);
+        $body.addClass("grow").removeClass("grown").removeClass("shrink");
+        setTimeout(function() {
+          $body.removeClass("grow").addClass("zoomed");
+          $target.addClass("grown");
+          clearInterval(zoomIntervalId);
+        }, 1000);
+    }
+
+    $currentTarget = $target;
+  };
+
+
+  $.zoomOut = function() {
+    if ($currentTarget == null)
+      return;
+
+    $currentTarget.removeClass("grown");
+    var offset = $currentTarget.data("offset");
+    //alert(offset.left);
+    var scale = $currentTarget.data("scale");
+
+    $(".dynamicCSS").remove();
+
+    if ($.browser.webkit) {
+      $("body").append("<style type='text/css' class='dynamicCSS'>"+
+        ".shrink {"+
+        "-webkit-transform-origin: "+offset.left+"px "+offset.top+"px; "+
+        "-webkit-animation-name: shrink-anim;"+
+        "}"+
+        "@-webkit-keyframes shrink-anim {"+
+        "0% { -webkit-transform: translate("+(-offset.left)+"px, "+(-offset.top)+"px) scale("+scale+");  }"+
+        "100% { -webkit-transform: scale(1) translate(0, 0); } "+
+        "}"+
+        "</style>");
+
+      $("body").removeClass("grow").removeClass("zoomed").addClass("shrink");
+      setTimeout(function() {
+        $("body").removeClass("shrink");
+      }, 1000);
+    } else if ($.browser.mozilla) {
+      $body.append("<style type='text/css' class='dynamicCSS'>"+
+        ".shrink {"+
+        emitProperties(css3({
+          "transform-origin": offset.left+"px "+offset.top+"px"
+        }))+
+        "}\n</style>");
+
+        var startTime = new Date().getTime();
+        var zoomIntervalId = setInterval(function() {
+          var ratio = 1-((new Date().getTime()-startTime)/1000);
+          $body.append("<style type='text/css' class='dynamicCSS'>"+
+          ".grow {"+
+            emitProperties(css3({
+              "transform-origin": offset.left+"px "+offset.top+"px",
+              "transform": "translate("+(-offset.left*ratio)+"px,"+(-offset.top*ratio)+"px) scale("+(1+(scale-1) * ratio)+")"
+            }))+ "}");
+        }, 25);
+        $body.addClass("grow").removeClass("grown").removeClass("shrink");
+        setTimeout(function() {
+          $body.removeClass("shrink");
+          clearInterval(zoomIntervalId);
+        }, 1000);
+    }
+
+    $currentTarget = null;
+  }
+
 
   function emitProperties(properties) {
     var parts = [];
@@ -80,71 +214,28 @@ $(function() {
     "animation-iteration-count": 1
   });
   
-  var shrunkCount = 0;
-  function shrinkElement(element, scale) {    
-    var offset = $(element).scale(scale).offset();
-    
-    shrunkCount ++;                   
-    
-    $("body").append("<style type='text/css'>"+
-      ".grow"+shrunkCount+" {"+
-      emitProperties(css3({
-        "transform-origin": offset.left+"px "+offset.top+"px",
-        "animation-name": "grow"+shrunkCount+"-anim"
-      }))+      
-      "}"+
-      "@-webkit-keyframes grow"+shrunkCount+"-anim {"+
-      "0% { -webkit-transform: scale(1) translate(0, 0); } "+
-      "100% { -webkit-transform: translate("+(-offset.left)+"px, "+(-offset.top+20*scale)+"px) scale("+scale+");  }"+
-      "} "+
-      ".zoomed"+shrunkCount+" { " +
-      emitProperties(css3({"transform": "translate("+(-offset.left)+"px, "+(-offset.top+20*scale)+"px) scale("+scale+")"}))+
-      "</style>");
-    
-    $("body").append("<style type='text/css'>"+
-      ".shrink"+shrunkCount+" {"+
-      "-webkit-transform-origin: "+offset.left+"px "+offset.top+"px; "+
-      "-webkit-animation-name: shrink"+shrunkCount+"-anim;"+
-      "}"+
-      "@-webkit-keyframes shrink"+shrunkCount+"-anim {"+
-      "100% { -webkit-transform: scale(1) translate(0, 0); } "+
-      "0% { -webkit-transform: translate("+(-offset.left)+"px, "+(-offset.top+20*scale)+"px) scale("+scale+");  }"+
-      "}"+
-      "</style>");
-  }
-    
-  $(".shrunk").each(function(i, e) {
-    shrinkElement(e, 20)
-    
-    var $e = $(e);
+
+
+
+  var scale = 20;
+  $(".shrunk").scale(20).each(function(i, e) {
+    var $e = $(e);   
       
     $e.click(function() {        
-      if ($body.hasClass("grow"+(i+1))) {          
-        return;
+      if ($currentTarget != null) {
+        return true;
       }
-        
-      $body.addClass("grow"+(i+1)).removeClass("grown"+(i+1)).removeClass("shrink");
-      setTimeout(function() {
-        $body.addClass("zoomed"+(i+1));
-        $e.addClass("grown");
-      }, 1000);   
-        
+      $.zoomTo(this);
       return false;
     });
       
     $e.find(".zoomOut").click(function() {        
-      if (!$body.hasClass("grow"+(i+1))) {          
-        return;
+      if ($currentTarget == null) {
+        return true;
       }
       
-      $e.removeClass("grown");
-              
-      $("body").removeClass("grow"+(i+1)).removeClass("zoomed"+(i+1)).addClass("shrink"+(i+1));
-      setTimeout(function() {
-        $("body").removeClass("shrink"+(i+1));
-      }, 1000); 
-       
-      return false;     
+      $.zoomOut();
+      return false;
     });
   });           
 });
