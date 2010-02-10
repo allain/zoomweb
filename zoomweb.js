699450b43132589bdbe690d83d5c37c920199f04
Allain Lalonde <allain@machete.ca>
2010-02-10 11:20:34 -0500

    Starting conversion into jQuery plugin

--- zoomweb.js
+++ zoomweb.js
@@ -1,60 +1,120 @@
 // on page load complete, fire off a jQuery json-p query
 // against Google web search
-$(function() {
-  var $body = $("body");
+$(function() {  
+  var CSS3properties = {
+    "transform" : true,
+    "transform-origin": true,
+    "animation-name": true,
+    "animation-duration": true,
+    "animation-iteration-count": true
+  };
   
-  var scale = 20;
-  
-  $body.css({
-      "-webkit-animation-duration" : "1s",      
-      "-webkit-animation-iteration-count": 1
+  function css3(properties) {
+    var result = {};
+    
+    for (propertyName in properties) {
+      if (properties.hasOwnProperty(propertyName)) {
+        if (CSS3properties[propertyName]) {
+          result["-webkit-" + propertyName] = properties[propertyName];
+          result["-moz-" + propertyName] = properties[propertyName];
+        } else {
+          result[propertyName] = properties[propertyName];
+        }
+      }
+    }
+    
+    return result;
+  }
+
+  (function(){
+    var oldCSS = $.fn.css;
+    $.fn.css = function(arg1, arg2) {
+      if (typeof(arg1) == "string") {
+        var newArg1 = {}
+        newArg1[arg1] = arg2;
+        arg1 = newArg1;
+        arg2 = null;
+      }
+
+      arg1 = css3(arg1);
+
+      return oldCSS.apply(this, [arg1, arg2]);
+    }
+  })();
+
+  $.fn.scale = function(s) {
+    for (var i=0; i<this.length; i++) {
+      var $e = $(this[i]);
+      var size = {
+        width: $e.width(),
+        height: $e.height()
+      };
+
+      $e.css({
+        "width": size.width,
+        "height": size.height,        
+        "transform": "scale("+(1/s)+")",
+        "transform-origin": "0 0"
+      }).wrap("<div class='shrunkContainer'/>").parent().css({
+        width: (size.width*(1/s)),
+        height:(size.height*(1/s)),
+        overflow: "hidden"
+      });
+    }
+
+    return this;
+  };
+
+  function emitProperties(properties) {
+    var parts = [];
+
+    for (propertyName in properties) {
+      parts.push(propertyName +":" + properties[propertyName]+";");
+    }
+    
+    return parts.join("\n");
+  }
+
+  var $body = $("body").css({
+    "animation-duration" : "1s",
+    "animation-iteration-count": 1
   });
   
   var shrunkCount = 0;
-  function shrinkElement(element, scale) {
-    var $e = $(element);   
-    var bodySize ={width: $("body").width(), height: $("body").height()};
-    var size = {width: $e.width(), height: $e.height()};
-        
-    $e.wrap("<div class='shrunkContainer'/>").parent().css({width: (size.width*(1/scale)), height:(size.height*(1/scale))});            
-    $e.css({
-      "width": size.width,
-      "height": size.height});
-    
-    alert($body.css("-webkit-transform"));
-    
-    var offset = $e.offset();
+  function shrinkElement(element, scale) {    
+    var offset = $(element).scale(scale).offset();
     
     shrunkCount ++;                   
     
     $("body").append("<style type='text/css'>"+
       ".grow"+shrunkCount+" {"+
-      "-webkit-transform-origin: "+offset.left+"px "+offset.top+"px; "+
-      "-webkit-animation-name: grow"+shrunkCount+"-anim;"+
+      emitProperties(css3({
+        "transform-origin": offset.left+"px "+offset.top+"px",
+        "animation-name": "grow"+shrunkCount+"-anim"
+      }))+      
       "}"+
-    "@-webkit-keyframes grow"+shrunkCount+"-anim {"+
-    "0% { -webkit-transform: scale(1) translate(0, 0); } "+
-    "100% { -webkit-transform: translate("+(-offset.left)+"px, "+(-offset.top+20*scale)+"px) scale("+scale+");  }"+
-    "} "+
-    ".zoomed"+shrunkCount+" { -webkit-transform: translate("+(-offset.left)+"px, "+(-offset.top+20*scale)+"px) scale("+scale+");  }"+
-    "</style>");
+      "@-webkit-keyframes grow"+shrunkCount+"-anim {"+
+      "0% { -webkit-transform: scale(1) translate(0, 0); } "+
+      "100% { -webkit-transform: translate("+(-offset.left)+"px, "+(-offset.top+20*scale)+"px) scale("+scale+");  }"+
+      "} "+
+      ".zoomed"+shrunkCount+" { " +
+      emitProperties(css3({"transform": "translate("+(-offset.left)+"px, "+(-offset.top+20*scale)+"px) scale("+scale+")"}))+
+      "</style>");
     
     $("body").append("<style type='text/css'>"+
       ".shrink"+shrunkCount+" {"+
       "-webkit-transform-origin: "+offset.left+"px "+offset.top+"px; "+
       "-webkit-animation-name: shrink"+shrunkCount+"-anim;"+
       "}"+
-    "@-webkit-keyframes shrink"+shrunkCount+"-anim {"+
-    "100% { -webkit-transform: scale(1) translate(0, 0); } "+
-    "0% { -webkit-transform: translate("+(-offset.left)+"px, "+(-offset.top+20*scale)+"px) scale("+scale+");  }"+
-    "}"+
-    "</style>");    
+      "@-webkit-keyframes shrink"+shrunkCount+"-anim {"+
+      "100% { -webkit-transform: scale(1) translate(0, 0); } "+
+      "0% { -webkit-transform: translate("+(-offset.left)+"px, "+(-offset.top+20*scale)+"px) scale("+scale+");  }"+
+      "}"+
+      "</style>");
   }
-  
-  $("body").append("<style type='text/css'>.shrunk { font-size: 100%; -webkit-transform-origin: 0 0; -webkit-transform: scale("+(1/scale)+"); text-align: left; }</style>");    
-  
+    
   $(".shrunk").each(function(i, e) {
-    shrinkElement(e, scale)
+    shrinkElement(e, 20)
     
     var $e = $(e);
       
@@ -65,8 +125,8 @@ $(function() {
         
       $body.addClass("grow"+(i+1)).removeClass("grown"+(i+1)).removeClass("shrink");
       setTimeout(function() {
-         $body.addClass("zoomed"+(i+1));
-         $e.addClass("grown");
+        $body.addClass("zoomed"+(i+1));
+        $e.addClass("grown");
       }, 1000);   
         
       return false;
@@ -80,8 +140,8 @@ $(function() {
       $e.removeClass("grown");
               
       $("body").removeClass("grow"+(i+1)).removeClass("zoomed"+(i+1)).addClass("shrink"+(i+1));
-       setTimeout(function() {
-         $("body").removeClass("shrink"+(i+1));
+      setTimeout(function() {
+        $("body").removeClass("shrink"+(i+1));
       }, 1000); 
        
       return false;     
