0258292c834d6dc5150c0b844f21ff395689ff0d
allain <allain.lalonde@gmail.com>
2011-12-06 16:09:37 +0000

    Stripping out redundant code and making use of CSS3 properties instead

--- zoomweb.js
+++ zoomweb.js
@@ -17,6 +17,7 @@
         if (CSS3properties[propertyName]) {
           result["-webkit-" + propertyName] = properties[propertyName];
           result["-moz-" + propertyName] = properties[propertyName];
+          result["-o-" + propertyName] = properties[propertyName];
         } else {
           result[propertyName] = properties[propertyName];
         }
@@ -59,7 +60,7 @@
         "height": height,        
         "transform": "scale("+(1/s)+")",
         "transform-origin": "0 0"
-      }).wrap("<div class='shrunkContainer'/>").parent().css({
+      }).wrap("<div class='zoomContainer'/>").parent().css({
         width: (width*(1/s)),
         height:(height*(1/s)),
         overflow: "hidden"
@@ -74,107 +75,34 @@
 
   var $currentTarget = null;
   
-  var parts = [];
-  function collectCSS(key, value) {
-    if (typeof(key) == "string") {
-      if (typeof(value) == "object") {
-        parts.push(key);
-        parts.push("{");
-        collectCSS(value);
-        parts.push("}");
-      } else {
-        parts.push(key);
-        parts.push(":");
-        parts.push(value);
-        parts.push(";");
-      }
-    } else {
-      for (propertyName in key) {
-        collectCSS(propertyName, key[propertyName]);
-      }
-    }
-  }
-
-  function buildCSS(key, value) {
-    parts=[];
-    collectCSS(key, value);     
-    return parts.join("");
-  }
-  
-  function buildStylesheet(key, value) {
-    return "<style type='text/css' class='dynamicCSS'>"+buildCSS(key, value)+"</style>";
-  }
-  
-  $.fn.appendStylesheet = function(rules, media) {
-    var html =  "<style type='text/css' class='dynamicCSS' media='" + (media || 'all') +"'>"+buildCSS(rules)+"</style>";
-    for (var i=0; i<this.length; i++) {
-      $(this[i]).append(html); 
-    }
+  function calculateScale($target) {
+      if ($target.length == 0)
+          return 1;
+      
+      var scale = $target.data("scale");
+      if (scale)
+          return scale * calculateScale($target.parent());
+      else
+          return calculateScale($target.parent())
   }
   
   $.zoomTo = function (target) {
     var $target = $(target);    
     var offset = $target.data("offset");   
-    var scale = $target.data("scale");
+    var scale = calculateScale($target);
   
     var $body = $("body");
 
-    $(".dynamicCSS").remove();
-
-    if ($.browser.webkit) {
-      $body.appendStylesheet({
-        ".grow": {
-          "-webkit-transform-origin": offset.left+"px "+offset.top+"px",
-          "-webkit-animation-name": "grow-anim",
-          "width": "100%"
-        },
-        "@-webkit-keyframes grow-anim": {
-          "0%": {"-webkit-transform": "translate(0, 0); scale(1)"},
-          "100%": {
-            "-webkit-transform": "translate("+(-offset.left)+"px, "+(-offset.top)+"px) scale("+scale+")"
-          }
-        },
-        ".zoomed": {
-          "-webkit-transform-origin": offset.left + "px "+offset.top + "px",
-          "-webkit-transform": "translate("+(-offset.left)+"px,"+(-offset.top)+"px) scale("+scale+")"
-        }
-      });        
-      
-      $body.addClass("grow").removeClass("grown").removeClass("shrink");
-      setTimeout(function() {
-        $body.removeClass("grow").addClass("zoomed");
-        $target.addClass("grown");
-      }, 1000);
-    } else if ($.browser.mozilla) {
-      $body.appendStylesheet({
-        ".zoomed": {
-          "-moz-transform-origin": offset.left + "px "+offset.top + "px",
-          "-moz-transform": "translate("+(-offset.left)+"px,"+(-offset.top)+"px) scale("+scale+")"        
-        }
+    if ($currentTarget)
+        $currentTarget.removeClass("zoomed");
+    
+    if ($.browser.webkit || $.browser.opera || $.browser.mozilla) {
+      $body.css({
+          "transform-origin": offset.left + "px "+offset.top + "px",
+          "transform": "translate("+(-offset.left)+"px,"+(-offset.top)+"px) scale("+scale+")"
       });
-
-      function emitAnimationStep(ratio) {
-        $body.appendStylesheet({
-            ".grow": {
-            "-moz-transform-origin": offset.left+"px "+offset.top+"px",
-            "-moz-transform": "translate("+(-offset.left*ratio)+"px,"+(-offset.top*ratio)+"px) scale("+(1+((scale-1) * ratio))+")"
-          }
-        });
-      }
-      emitAnimationStep(0);
       
-      var startTime = new Date().getTime();
-      var zoomIntervalId = setInterval(function() {
-        var ratio = (new Date().getTime()-startTime)/1000;
-        emitAnimationStep(ratio);
-      }, 25);
-
-      $body.addClass("grow").removeClass("grown").removeClass("shrink");
-      setTimeout(function() {
-        $body.removeClass("grow").addClass("zoomed");
-        $target.addClass("grown");
-        clearInterval(zoomIntervalId);
-      }, 1000);
+      $target.addClass("zoomed");
     }
 
     $currentTarget = $target;
@@ -185,67 +113,22 @@
     if ($currentTarget == null)
       return;
 
-    $currentTarget.removeClass("grown");
+    $currentTarget.removeClass("zoomed");
+
     var offset = $currentTarget.data("offset");
     var scale = $currentTarget.data("scale");
 
-    $(".dynamicCSS").remove();
+    //$(".dynamicCSS").remove();
 
-    if ($.browser.webkit) {
-      $("body").appendStylesheet({        
-        ".shrink": {
-          "-webkit-transform-origin": offset.left+"px "+offset.top+"px; ",
-          "-webkit-animation-name": "shrink-anim;"
-        },
-        "@-webkit-keyframes shrink-anim": {
-          "0%": {
-            "-webkit-transform": "translate("+(-offset.left)+"px, "+(-offset.top)+"px) scale("+scale+")"
-          },
-          "100%": { 
-            "-webkit-transform": "scale(1) translate(0, 0)"
-          }
-        }
-      });
+    var $body = $("body");
 
-      $("body").removeClass("grow").removeClass("zoomed").addClass("shrink");
-      setTimeout(function() {
-        $("body").removeClass("shrink");
-      }, 1000);
-    } else if ($.browser.mozilla) {
-       $(".dynamicCSS").remove();
-
-      function emitAnimationStep(ratio) {
-        $("body").appendStylesheet({
-          ".shrink": {
-            "-moz-transform-origin": offset.left+"px "+offset.top+"px",
-            "-moz-transform": "translate("+(-offset.left*ratio)+"px,"+(-offset.top*ratio)+"px) scale("+(1+(scale-1) * ratio)+")"
-          }
-        });
-      }
-
-      emitAnimationStep(1);
-      
-      var startTime = new Date().getTime();
-      var zoomIntervalId = setInterval(function() {
-        var ratio = 1-((new Date().getTime()-startTime)/1000);
-        emitAnimationStep(ratio)
-      }, 50);
-
-      $("body").removeClass("grow").removeClass("grown").addClass("shrink");
-      
-      setTimeout(function() {
-        $("body").removeClass("shrink");
-        clearInterval(zoomIntervalId);
-      }, 1000);
+    if ($.browser.webkit || $.browser.opera || $.browser.mozilla) {
+       $body.css({
+          "transform-origin": offset.left + "px "+offset.top + "px",
+          "transform": "translate(0px,0px) scale(1)"
+       });
     }
 
     $currentTarget = null;
   }
-
-  $(function() {
-    $("body").css({
-      "animation-duration" : "1s",
-      "animation-iteration-count": 1
-    });
-  })
 })(jQuery);
